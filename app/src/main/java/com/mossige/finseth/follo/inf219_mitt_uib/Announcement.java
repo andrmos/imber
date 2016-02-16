6aa30acd2c927aab0d1c591cd4b1bced60e7bb14
Patrick Finseth <patrick_j.o_92@hotmail.com>
2016-02-16 11:52:15 +0100

    Implemented course and announcement class

---
+++ app/src/main/java/com/mossige/finseth/follo/inf219_mitt_uib/Announcement.java
@@ -0,0 +1,13 @@
+package com.mossige.finseth.follo.inf219_mitt_uib;
+
+/**
+ * Created by PatrickFinseth on 16.02.16.
+ */
+public class Announcement {
+
+    private int id;
+
+    public Announcement(int id){
+        this.id = id;
+    }
+}
---
+++ app/src/main/java/com/mossige/finseth/follo/inf219_mitt_uib/Course.java
@@ -0,0 +1,36 @@
+package com.mossige.finseth.follo.inf219_mitt_uib;
+
+/**
+ * Created by PatrickFinseth on 16.02.16.
+ */
+public class Course {
+
+    private int id;
+    private String name;
+    private String calender;
+    private Announcement announcement;
+
+    public Course(int id, String name, String calender){
+        this.id = id;
+        this.name = name;
+        this.calender = calender;
+        announcement = new Announcement(id);
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getCalenderUrl(){
+        return calender;
+    }
+
+    public Announcement getAnnouncement(){
+        return announcement;
+    }
+
+}
