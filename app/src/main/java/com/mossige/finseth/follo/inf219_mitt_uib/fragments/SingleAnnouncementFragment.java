f7308956c3150e1e4e52ce768ba2cd491336dcb0
ofo002 <ofo002@student.uib.no>
2016-03-17 03:24:32 +0100

    Single conversation alphaversion up and running

--- app/src/main/java/com/mossige/finseth/follo/inf219_mitt_uib/fragments/SingleAnnouncementFragment.java
+++ app/src/main/java/com/mossige/finseth/follo/inf219_mitt_uib/fragments/SingleAnnouncementFragment.java
@@ -31,6 +31,17 @@ public class SingleAnnouncementFragment extends Fragment{
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_announcement, container, false);
 
+        TextView title_tv = (TextView) view.findViewById(R.id.single_announcement_title);
+        TextView message_tv = (TextView) view.findViewById(R.id.single_announcement_message);
+        TextView sender_tv = (TextView) view.findViewById(R.id.single_announcement_sender);
+        TextView date_tv = (TextView) view.findViewById(R.id.single_announcement_date);
+
+        // Set text to every TextView in fragment_announcement.xml
+        title_tv.setText(""+ getArguments().get("title"));
+        message_tv.setText(getArguments().get("message").toString());
+        sender_tv.setText("" + getArguments().get("sender"));
+        date_tv.setText(parseDate(getArguments().get("date").toString()));
+
         return view;
     }
 
