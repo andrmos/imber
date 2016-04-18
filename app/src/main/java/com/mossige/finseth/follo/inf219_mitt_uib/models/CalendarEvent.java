8e78f85b3a2391f80daea356bc989344e5c302e5
patfinzz <patrick.finseth@outlook.com>
2016-04-18 18:54:47 +0200

    New model for calendar events

--- app/src/main/java/com/mossige/finseth/follo/inf219_mitt_uib/models/CalendarEvent.java
+++ app/src/main/java/com/mossige/finseth/follo/inf219_mitt_uib/models/CalendarEvent.java
@@ -10,8 +10,14 @@ public class CalendarEvent {
     private Date startDate;
     private Date endDate;
     private String name;
-    private String summary;
     private String location;
+
+    public CalendarEvent(String name, String startDate, String endDate, String location){
+        this.name = name;
+        this.startDate = parseDateString(startDate);
+        this.endDate = parseDateString(endDate);
+        this.location = location;
+    }
 
     public CalendarEvent(String name, Date startDate, Date endDate, String location){
         this.name = name;
@@ -34,6 +40,15 @@ public class CalendarEvent {
 
     public String getLocation() {
         return location;
+    }
+
+    private static Date parseDateString(String date){
+        int year = Integer.parseInt(date.substring(0,4));
+        int month = Integer.parseInt(date.substring(5,7));
+        int day = Integer.parseInt(date.substring(8,10));
+        int hour = Integer.parseInt(date.substring(11,13));
+        int min = Integer.parseInt(date.substring(14,16));
+        return new Date(year, month, day, hour, min);
     }
 
     @Override
