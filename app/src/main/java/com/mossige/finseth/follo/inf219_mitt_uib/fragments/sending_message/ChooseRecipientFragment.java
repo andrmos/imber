cfe5cdb5dcbb9c4bec394ca15054b7d87af065bf
patfinzz <patrick.finseth@outlook.com>
2016-05-12 10:47:47 +0200

    Made a layout file for smooth progress bar

--- app/src/main/java/com/mossige/finseth/follo/inf219_mitt_uib/fragments/sending_message/ChooseRecipientFragment.java
+++ app/src/main/java/com/mossige/finseth/follo/inf219_mitt_uib/fragments/sending_message/ChooseRecipientFragment.java
@@ -160,7 +160,7 @@ public class ChooseRecipientFragment extends Fragment {
         initCourseSpinner();
         initRecycleView();
 
-        progressBar = (SmoothProgressBar) rootView.findViewById(R.id.progressBar2);
+        progressBar = (SmoothProgressBar) rootView.findViewById(R.id.progressbar);
         if (loaded) {
             progressBar.setVisibility(View.GONE);
         } else {
