88dd3552172f5d784bc632c20f748cc1f21a0df7
andrmos <mossige.a@gmail.com>
2016-12-30 00:04:59 +0100

    Append access token to every http request

--- app/src/main/java/com/mossige/finseth/follo/inf219_mitt_uib/network/retrofit/ServiceGenerator.java
+++ app/src/main/java/com/mossige/finseth/follo/inf219_mitt_uib/network/retrofit/ServiceGenerator.java
@@ -1,6 +1,17 @@
 package com.mossige.finseth.follo.inf219_mitt_uib.network.retrofit;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.mossige.finseth.follo.inf219_mitt_uib.R;
+
+import java.io.IOException;
+
+import okhttp3.HttpUrl;
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -17,7 +28,28 @@ public class ServiceGenerator {
             .baseUrl(API_BASE_URL)
             .addConverterFactory(GsonConverterFactory.create());
 
-    public static <S> S createService(Class<S> serviceClass) {
+    public static <S> S createService(Class<S> serviceClass, final String accessToken) {
+
+        // Add access_token to every request
+        httpClient.addInterceptor(new Interceptor() {
+            @Override
+            public Response intercept(Chain chain) throws IOException {
+                Request original = chain.request();
+                HttpUrl originalHttpUrl = original.url();
+
+                HttpUrl url = originalHttpUrl.newBuilder()
+                        .addQueryParameter("access_token", accessToken)
+                        .build();
+
+                // Request customization: add request headers
+                Request.Builder requestBuilder = original.newBuilder()
+                        .url(url);
+
+                Request request = requestBuilder.build();
+                return chain.proceed(request);
+            }
+        });
+
         Retrofit retrofit = builder.client(httpClient.build()).build();
         return retrofit.create(serviceClass);
     }
