28eeee0b1c35e11b0dbac821bfc39abfd3252834
andrmos <mossige.a@gmail.com>
2016-12-30 00:49:21 +0100

    Add getCourses to retrofit interface

--- app/src/main/java/com/mossige/finseth/follo/inf219_mitt_uib/network/retrofit/MittUibClient.java
+++ app/src/main/java/com/mossige/finseth/follo/inf219_mitt_uib/network/retrofit/MittUibClient.java
@@ -1,6 +1,9 @@
 package com.mossige.finseth.follo.inf219_mitt_uib.network.retrofit;
 
+import com.mossige.finseth.follo.inf219_mitt_uib.models.Course;
 import com.mossige.finseth.follo.inf219_mitt_uib.models.User;
+
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
@@ -11,7 +14,18 @@ import retrofit2.http.GET;
 
 public interface MittUibClient {
 
+    /**
+     * @return The logged in users profile data.
+     */
     @GET("users/self/profile")
     Call<User> getProfile();
+
+    /**
+     * @return The active courses of the logged in user.
+     */
+    @GET("courses")
+    Call<List<Course>> getCourses();
+
+
 
 }
