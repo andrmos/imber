68b189541e0c2a0b82cbc7a38185ad668682b5ad
andrmos <mossige.a@gmail.com>
2017-01-07 18:04:12 +0100

    Replace volley request validating access token with retrofit call

--- app/src/main/java/com/mossige/finseth/follo/inf219_mitt_uib/activities/LoginActivityWithAccessToken.java
+++ app/src/main/java/com/mossige/finseth/follo/inf219_mitt_uib/activities/LoginActivityWithAccessToken.java
@@ -6,119 +6,93 @@ import android.content.SharedPreferences;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
 import com.mossige.finseth.follo.inf219_mitt_uib.R;
+import com.mossige.finseth.follo.inf219_mitt_uib.models.Course;
 import com.mossige.finseth.follo.inf219_mitt_uib.network.PrivateConstants;
-import com.mossige.finseth.follo.inf219_mitt_uib.network.RequestQueueHandler;
-import com.mossige.finseth.follo.inf219_mitt_uib.network.UrlEndpoints;
+import com.mossige.finseth.follo.inf219_mitt_uib.network.retrofit.MittUibClient;
+import com.mossige.finseth.follo.inf219_mitt_uib.network.retrofit.ServiceGenerator;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import java.util.List;
 
-import static java.lang.Class.forName;
+import retrofit2.Call;
+import retrofit2.Callback;
 
 public class LoginActivityWithAccessToken extends AppCompatActivity {
 
-    private boolean valid;
-    private EditText token;
+    private EditText inputField;
     private SharedPreferences sharedPreferences;
-    private Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_activity_with_access_token);
         sharedPreferences = getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE);
 
-        if(!PrivateConstants.ACCESS_TOKEN.equals("")) {
-            SharedPreferences.Editor editor = sharedPreferences.edit();
-            editor.putString("access_token", PrivateConstants.ACCESS_TOKEN);
-            editor.apply();
+        // TODO Remove
+        // Hard coded access token for development
+        if(!PrivateConstants.ACCESS_TOKEN.isEmpty()) {
+            storeToken(PrivateConstants.ACCESS_TOKEN);
+            changeActivity();
 
-            Intent intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
-        }else{
+        // Access token is stored in shared preference
+        } else if (sharedPreferences.contains("access_token")) {
+            changeActivity();
 
-            if(sharedPreferences.contains("access_token")) {
-                validateAccessToken(sharedPreferences.getString("access_token",""));
-            }
+        // Prompt for new access token
+        } else {
+            setContentView(R.layout.activity_login_activity_with_access_token);
+
+            inputField = (EditText) findViewById(R.id.token);
+            Button login = (Button) findViewById(R.id.login_button);
+            login.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String accessToken = inputField.getText().toString();
+                    validateAccessToken(accessToken);
+                }
+            });
         }
-
-        valid =false;
-
-        token = (EditText) findViewById(R.id.token);
-
-        login = (Button) findViewById(R.id.login_button);
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                validateAccessToken(token.getText().toString());
-            }
-        });
-
     }
 
+    // TODO Change to retrieving profile, and bundle it to MainActivity
     private void validateAccessToken(final String token){
-
-        oldReq(token);
-    }
-
-    private void oldReq(final String token) {
-        final JsonArrayRequest validate = new JsonArrayRequest(Request.Method.GET, UrlEndpoints.validateTokenURL(token), (String) null, new Response.Listener<JSONArray>() {
-
+        MittUibClient client = ServiceGenerator.createService(MittUibClient.class, getApplicationContext());
+        Call<List<Course>> call = client.getCourses();
+        call.enqueue(new Callback<List<Course>>() {
             @Override
-            public void onResponse(JSONArray response) {
-
-                boolean errors = false;
-
-                for(int i = 0; i < response.length(); i++){
-                    try {
-                        JSONObject obj = response.getJSONObject(i);
-                        if(obj.has("errors")){
-                            Snackbar snackbar = Snackbar.make(findViewById(R.id.login_access_token), "Ikke gyldig aksessnøkkel", Snackbar.LENGTH_INDEFINITE);
-                            snackbar.show();
-
-                            EditText tokenText = (EditText) findViewById(R.id.token);
-
-                            tokenText.setText("");
-
-                            errors = true;
-                            break;
-                        }
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
+            public void onResponse(Call<List<Course>> call, retrofit2.Response<List<Course>> response) {
+                if (response.isSuccessful()) {
+                    storeToken(token);
+                    changeActivity();
+                } else {
+                    showSnackbar();
                 }
-
-                if(!errors){
-                    valid = true;
-
-                    SharedPreferences.Editor editor = sharedPreferences.edit();
-                    editor.putString("access_token", token);
-                    editor.apply();
-
-                    Intent intent = new Intent(getApplication(),MainActivity.class);
-                    startActivity(intent);
-                }
-
             }
-        }, new Response.ErrorListener() {
+
             @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.i("login", "onErrorResponse: " + error.toString());
+            public void onFailure(Call<List<Course>> call, Throwable t) {
+                showSnackbar();
             }
         });
+    }
 
-        RequestQueueHandler.getInstance(this).addToRequestQueue(validate);
+    private void changeActivity() {
+        Intent intent = new Intent(getApplication(), MainActivity.class);
+        startActivity(intent);
+    }
+
+    private void storeToken(String token) {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("access_token", token);
+        editor.apply();
+    }
+
+    private void showSnackbar() {
+        Snackbar snackbar = Snackbar.make(findViewById(R.id.login_access_token), R.string.error_invalid_access_token, Snackbar.LENGTH_SHORT);
+        snackbar.show();
     }
 
 }
