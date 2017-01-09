80418315e3ff26fe211fea44fe7faaa00e561602
andrmos <mossige.a@gmail.com>
2017-01-09 20:37:18 +0100

    Create simple view holder for File objects

---
+++ app/src/main/java/com/mossige/finseth/follo/inf219_mitt_uib/card_view_holders/FileViewHolder.java
@@ -0,0 +1,21 @@
+package com.mossige.finseth.follo.inf219_mitt_uib.card_view_holders;
+
+import android.view.View;
+import android.widget.TextView;
+
+import com.mossige.finseth.follo.inf219_mitt_uib.R;
+
+/**
+ * Created by andre on 09.01.17.
+ */
+
+public class FileViewHolder extends GeneralViewHolder {
+
+    public TextView name;
+
+    public FileViewHolder(View v) {
+        super(v);
+        name = (TextView) v.findViewById(R.id.name);
+    }
+
+}
