c023f8df5589985cb9e8a72c59148328e7025fca
andrmos <mossige.a@gmail.com>
2017-01-09 17:20:56 +0100

    Create file browser fragment

---
+++ app/src/main/java/com/mossige/finseth/follo/inf219_mitt_uib/fragments/FileBrowserFragment.java
@@ -0,0 +1,27 @@
+package com.mossige.finseth.follo.inf219_mitt_uib.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mossige.finseth.follo.inf219_mitt_uib.R;
+
+/**
+ * Created by andre on 09.01.2017.
+ */
+public class FileBrowserFragment extends Fragment {
+
+    public FileBrowserFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_file_browser, container, false);
+    }
+}
