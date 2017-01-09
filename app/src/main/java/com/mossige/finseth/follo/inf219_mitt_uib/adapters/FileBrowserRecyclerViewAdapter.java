2a3b2d0a3fcc410abe0de2e324a5a1cfb6baae0b
andrmos <mossige.a@gmail.com>
2017-01-09 19:25:24 +0100

    Create empty recycler view adapter for file browser

---
+++ app/src/main/java/com/mossige/finseth/follo/inf219_mitt_uib/adapters/FileBrowserRecyclerViewAdapter.java
@@ -0,0 +1,24 @@
+package com.mossige.finseth.follo.inf219_mitt_uib.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by andre on 09.01.17.
+ */
+public class FileBrowserRecyclerViewAdapter extends android.support.v7.widget.RecyclerView.Adapter {
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
