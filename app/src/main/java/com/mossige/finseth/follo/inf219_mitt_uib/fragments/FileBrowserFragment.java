7a8d31db9b549f97a22125a616155c62d34ad764
andrmos <mossige.a@gmail.com>
2017-01-19 20:41:49 +0100

    Show prompt dialog when file size is big

--- app/src/main/java/com/mossige/finseth/follo/inf219_mitt_uib/fragments/FileBrowserFragment.java
+++ app/src/main/java/com/mossige/finseth/follo/inf219_mitt_uib/fragments/FileBrowserFragment.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.app.DownloadManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
@@ -16,6 +17,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -56,8 +58,8 @@ public class FileBrowserFragment extends Fragment implements ActivityCompat.OnRe
 
     private static final String CURRENT_FOLDER_ID_KEY = "currentFolderId";
     public static final String COURSE_KEY = "course";
-    // If file size is above this limit, prompt for confirmation: 30 MB
-    private static final long FILE_SIZE_WARNING_TRESHHOLD = 30000000;
+    // If file size is above this limit, prompt for confirmation: 50 MB TODO Tweak size
+    private static final long FILE_SIZE_WARNING_TRESHHOLD = 50000000;
 
     private RecyclerView mainList;
     private RecyclerView.Adapter mAdapter;
@@ -333,27 +335,54 @@ public class FileBrowserFragment extends Fragment implements ActivityCompat.OnRe
     }
 
     // TODO Download file to cache folder instead of external?
-    public void downloadFile(File file) {
+    public void downloadFile(final File file) {
 
         // File size in bytes
         if (file.getSize() > FILE_SIZE_WARNING_TRESHHOLD) {
-            // TODO Implement
+            showPromt(file);
 
         } else {
-            DownloadManager manager = (DownloadManager) getContext().getSystemService(DOWNLOAD_SERVICE);
-            DownloadManager.Request request = new DownloadManager.Request(Uri.parse(file.getUrl()));
-            request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, file.getFileName());
-
-            String permission = Manifest.permission.WRITE_EXTERNAL_STORAGE;
-            if (PermissionUtils.isPermissionGranted(permission, getActivity())) {
-                enqueuedDownload = manager.enqueue(request);
-
-            } else {
-                requestPermissions(new String[]{permission}, 1);
-            }
+            enqueueDownload(file);
         }
 
 
+    }
+
+    private void showPromt(final File file) {
+        long tresholdMb = file.getSize() / 1000000;
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage(getString(R.string.file_size_prompt_message, tresholdMb))
+                .setTitle(R.string.file_size_prompt_title);
+
+        builder.setPositiveButton(R.string.file_size_promt_positive_button, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                enqueueDownload(file);
+            }
+        });
+        builder.setNegativeButton(R.string.file_size_promt_negative_button, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // Do nothing
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    // TODO Refactor and change responsibility
+    private void enqueueDownload(File file) {
+        DownloadManager manager = (DownloadManager) getContext().getSystemService(DOWNLOAD_SERVICE);
+        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(file.getUrl()));
+        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, file.getFileName());
+
+        String permission = Manifest.permission.WRITE_EXTERNAL_STORAGE;
+        if (PermissionUtils.isPermissionGranted(permission, getActivity())) {
+            enqueuedDownload = manager.enqueue(request);
+
+        } else {
+            requestPermissions(new String[]{permission}, 1);
+        }
     }
 
     private class DownloadComplete extends BroadcastReceiver {
@@ -401,7 +430,7 @@ public class FileBrowserFragment extends Fragment implements ActivityCompat.OnRe
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
-            downloadFile(clickedFile);
+            enqueueDownload(clickedFile);
         }
     }
 }
