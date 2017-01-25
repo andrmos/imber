bfcf587d44a3a581d9512caf76afbe2199e1a7a4
andrmos <mossige.a@gmail.com>
2017-01-25 14:15:28 +0100

    Add 'Licenses' menu option in about fragment

--- app/src/main/java/com/mossige/finseth/follo/inf219_mitt_uib/fragments/AboutFragment.java
+++ app/src/main/java/com/mossige/finseth/follo/inf219_mitt_uib/fragments/AboutFragment.java
@@ -1,18 +1,20 @@
 package com.mossige.finseth.follo.inf219_mitt_uib.fragments;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.webkit.WebView;
 
 import com.mossige.finseth.follo.inf219_mitt_uib.R;
-import com.mossige.finseth.follo.inf219_mitt_uib.models.Course;
-
-import java.util.ArrayList;
 
 
 /**
@@ -21,6 +23,8 @@ import java.util.ArrayList;
 public class AboutFragment extends Fragment {
 
     private static final String TAG = "AboutFragment";
+    public static final String LICENSES_URL = "https://github.com/andrmos/mitt-uib-android-app/blob/master/LICENSE.md";
+    private WebView webView;
 
     public AboutFragment() {
         // Required empty public constructor
@@ -29,16 +33,36 @@ public class AboutFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-
         getActivity().setTitle(R.string.about_title);
-
         View view = inflater.inflate(R.layout.fragment_about, container, false);
-
+        setHasOptionsMenu(true);
         return view;
     }
 
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.about_menu, menu);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
 
+        if (id == R.id.licenses) {
+            showLicenses();
+            return true;
+        }
 
+        return false;
+    }
 
+    /**
+     * Show the licenses in the device web browser.
+     */
+    private void showLicenses() {
+        String url = LICENSES_URL;
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
+    }
 }
