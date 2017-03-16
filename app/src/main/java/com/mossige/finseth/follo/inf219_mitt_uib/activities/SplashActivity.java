89bee432ec15046ce38ae4cb965b80ed2c239eb3
andrmos <mossige.a@gmail.com>
2017-03-16 17:04:13 +0100

    Go to main activity from splash if access token is present

--- app/src/main/java/com/mossige/finseth/follo/inf219_mitt_uib/activities/SplashActivity.java
+++ app/src/main/java/com/mossige/finseth/follo/inf219_mitt_uib/activities/SplashActivity.java
@@ -1,8 +1,23 @@
 package com.mossige.finseth.follo.inf219_mitt_uib.activities;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.google.gson.Gson;
+import com.mossige.finseth.follo.inf219_mitt_uib.R;
+import com.mossige.finseth.follo.inf219_mitt_uib.models.Course;
+import com.mossige.finseth.follo.inf219_mitt_uib.models.User;
+import com.mossige.finseth.follo.inf219_mitt_uib.retrofit.MittUibClient;
+import com.mossige.finseth.follo.inf219_mitt_uib.retrofit.ServiceGenerator;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,9 +25,18 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // TODO Merge SplashActivity and LoginActivity, and fetch profile while splash is showing.
-        Intent intent = new Intent(this, LoginActivityWithAccessToken.class);
-        startActivity(intent);
+        SharedPreferences sharedPreferences = getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+        if (sharedPreferences.contains("access_token")) {
+            // Launch MainActivity
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+
+        } else {
+            // Launch login
+            Intent intent = new Intent(this, LoginActivityWithAccessToken.class);
+            startActivity(intent);
+        }
+
         finish();
     }
 }
