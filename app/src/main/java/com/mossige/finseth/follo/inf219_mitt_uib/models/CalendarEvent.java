90f16a26e06a4147d1a46828a03818b2a6fbe07d
andrmos <mossige.a@gmail.com>
2017-03-21 20:22:41 +0100

    Add methods for trimming title and location

--- app/src/main/java/com/mossige/finseth/follo/inf219_mitt_uib/models/CalendarEvent.java
+++ app/src/main/java/com/mossige/finseth/follo/inf219_mitt_uib/models/CalendarEvent.java
@@ -14,7 +14,7 @@ public class CalendarEvent implements Comparable<CalendarEvent> {
 
     private String id;
     private String title;
-    @SerializedName("location_address")
+    @SerializedName("location_name")
     private String location;
     @SerializedName("start_at")
     private DateTime startDate;
@@ -22,13 +22,18 @@ public class CalendarEvent implements Comparable<CalendarEvent> {
     private DateTime endDate;
     private boolean hidden;
 
-    public CalendarEvent(int id, String title, String location, DateTime startDate, DateTime endDate, boolean hidden) {
-//        this.id = id;
+    public CalendarEvent(String id, String title, String location, DateTime startDate, DateTime endDate, boolean hidden) {
+        this.id = id;
         this.title = title;
         this.location = location;
         this.startDate = startDate;
         this.endDate = endDate;
         this.hidden = hidden;
+    }
+
+
+    public boolean isHidden() {
+        return hidden;
     }
 
     public String getId() {
@@ -39,8 +44,16 @@ public class CalendarEvent implements Comparable<CalendarEvent> {
         this.id = id;
     }
 
-    public String getTitle(){
+    public String getTitle() {
         return title;
+    }
+
+    public String getTrimmedTitle() {
+        return trimEventName(title);
+    }
+
+    public String getTrimmedLocation() {
+        return trimLocation(location);
     }
 
     public DateTime getStartDate() {
@@ -57,52 +70,57 @@ public class CalendarEvent implements Comparable<CalendarEvent> {
 
     /**
      * Remove brackets and unnecessary title info
+     *
      * @param name
      * @return
      */
-    private String trimEventName(String name){
-        // TODO trim event name and location
+    private String trimEventName(String name) {
         String[] splitArray = name.split(" ");
         String trimedName = "";
 
-        if(splitArray[0].startsWith("[")) {
+        if (splitArray[0].startsWith("[")) {
             String course_code = "";
-            for (int i = 1; i < splitArray[0].length(); i++){
-                if(splitArray[0].charAt(i) != ']'){
+            for (int i = 1; i < splitArray[0].length(); i++) {
+                if (splitArray[0].charAt(i) != ']') {
                     course_code += splitArray[0].charAt(i);
                 }
 
             }
             trimedName += course_code;
             trimedName += " " + splitArray[1];
-        }else{
+        } else {
             trimedName = name;
         }
 
         return trimedName;
     }
 
-    private String trimLocation(String location){
-        String trimmedLocation = location.trim();
+    private String trimLocation(String location) {
+        if (location != null) {
 
-        if(trimmedLocation.startsWith("(")){
-            trimmedLocation = trimmedLocation.substring(1,trimmedLocation.length());
+            String trimmedLocation = location.trim();
 
-            if(location.endsWith(")")){
-                trimmedLocation = trimmedLocation.substring(0, trimmedLocation.length() - 1);
-                return trimmedLocation;
+            if (trimmedLocation.startsWith("(")) {
+                trimmedLocation = trimmedLocation.substring(1, trimmedLocation.length());
+
+                if (location.endsWith(")")) {
+                    trimmedLocation = trimmedLocation.substring(0, trimmedLocation.length() - 1);
+                    return trimmedLocation;
+                }
             }
+
+
+            return location.trim();
+        } else {
+            return "";
         }
-
-
-        return location.trim();
     }
 
     @Override
-    public int compareTo(CalendarEvent event){
-        if(this.getStartDate().equals(event.getStartDate())) {
+    public int compareTo(CalendarEvent event) {
+        if (this.getStartDate().equals(event.getStartDate())) {
             return 0;
-        }else if(this.getStartDate().gt(event.getStartDate())){
+        } else if (this.getStartDate().gt(event.getStartDate())) {
             return 1;
         } else {
             return -1;
@@ -123,8 +141,7 @@ public class CalendarEvent implements Comparable<CalendarEvent> {
     @Override
     public String toString() {
         return "CalendarEvent{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
+                "title='" + title + '\'' +
                 ", location='" + location + '\'' +
                 ", startDate=" + startDate +
                 ", endDate=" + endDate +
