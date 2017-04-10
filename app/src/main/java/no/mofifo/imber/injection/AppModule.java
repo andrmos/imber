b4eb40dff80f89a987a99402f9df213695a0305c
andrmos <mossige.a@gmail.com>
2017-04-10 14:57:29 +0200

    Create module providing application context

---
+++ app/src/main/java/no/mofifo/imber/injection/AppModule.java
@@ -0,0 +1,29 @@
+package no.mofifo.imber.injection;
+
+import android.app.Application;
+import android.content.Context;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+
+/**
+ * Dagger module providing a reference to the application context.
+ * Created by andre on 10.04.17.
+ */
+@Module
+public class AppModule {
+
+    private Application application;
+
+    public AppModule(Application application) {
+        this.application= application;
+    }
+
+    @Provides
+    @Singleton
+    Context providesApplication() {
+        return application;
+    }
+}
