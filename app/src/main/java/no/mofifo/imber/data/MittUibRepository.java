1c0594012a717321e1976e5c2fbad7f9a1385599
andrmos <mossige.a@gmail.com>
2017-04-12 15:55:58 +0200

    Implement simple request getting favorite courses

--- app/src/main/java/no/mofifo/imber/data/MittUibRepository.java
+++ app/src/main/java/no/mofifo/imber/data/MittUibRepository.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import no.mofifo.imber.models.Course;
 import no.mofifo.imber.retrofit.MittUibClient;
+import retrofit2.Call;
+import retrofit2.Response;
 
 /**
  * Created by andre on 10.04.17.
@@ -18,8 +20,23 @@ public class MittUibRepository implements MittUibDataSource {
     }
 
     @Override
-    public void getFavoriteCourses(Callback<List<Course>> callback) {
-        
+    public void getFavoriteCourses(final Callback<List<Course>> callback) {
+        Call<List<Course>> call = client.getFavoriteCourses();
+        call.enqueue(new retrofit2.Callback<List<Course>>() {
+            @Override
+            public void onResponse(Call<List<Course>> call, Response<List<Course>> response) {
+                if (response.isSuccessful()) {
+                    callback.onSuccess(response.body());
+                } else {
+                    callback.onFailure();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Course>> call, Throwable t) {
+                callback.onFailure();
+            }
+        });
     }
 
 }
