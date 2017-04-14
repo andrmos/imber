3f3a0f5a9472281223a13067dc3a4acce7817827
andrmos <mossige.a@gmail.com>
2017-04-14 16:10:09 +0200

    Create dagger component for courses feature

---
+++ app/src/main/java/no/mofifo/imber/course/CoursesComponent.java
@@ -0,0 +1,16 @@
+package no.mofifo.imber.course;
+
+import dagger.Component;
+import no.mofifo.imber.injection.ApiComponent;
+import no.mofifo.imber.injection.FragmentScoped;
+
+/**
+ * Created by andre on 12.04.17.
+ */
+@FragmentScoped
+@Component(dependencies = ApiComponent.class, modules = CoursesPresenterModule.class)
+public interface CoursesComponent {
+
+    void inject(CoursesFragment coursesFragment);
+
+}
