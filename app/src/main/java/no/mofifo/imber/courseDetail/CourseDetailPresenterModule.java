8a35a8b216d422e9d4d075c7144729b1fe13d1db
andrmos <mossige.a@gmail.com>
2017-11-21 22:54:31 +0100

    Remove provider to old list adapter

--- app/src/main/java/no/mofifo/imber/courseDetail/CourseDetailPresenterModule.java
+++ app/src/main/java/no/mofifo/imber/courseDetail/CourseDetailPresenterModule.java
@@ -1,12 +1,8 @@
 package no.mofifo.imber.courseDetail;
 
-import java.util.ArrayList;
-
 import dagger.Module;
 import dagger.Provides;
 import no.mofifo.imber.injection.FragmentScoped;
-import no.mofifo.imber.models.Announcement;
-import no.mofifo.imber.models.CalendarEvent;
 
 /**
  * Dagger module defining dependencies injectable within the fragment displaying a list of courses.
@@ -26,11 +22,4 @@ public class CourseDetailPresenterModule {
     CourseDetailView provideView() {
         return view;
     }
-
-    @Provides
-    @FragmentScoped
-    CourseDetailAdapter provideCourseDetailAdapter() {
-        return new CourseDetailAdapter(new ArrayList<Announcement>(), new ArrayList<CalendarEvent>());
-    }
-
 }
